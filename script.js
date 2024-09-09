20d808321746027f8c65f0b9af15a854176260bb
DevakirubaiJohn <johndevakirubai21@gmail.com>
2024-09-09 19:47:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    // check if year is divisible by 4
+    if (year % 4 === 0) {
+        // check if year is divisible by 100
+        if (year % 100 === 0) {
+            // check if year is divisible by 400
+            if (year % 400 === 0) {
+                // it's a leap year
+                return 366;
+            } else {
+                // it's not a leap year
+                return 365;
+            }
+        } else {
+            // it's a leap year
+            return 366;
+        }
+    } else {
+        // it's not a leap year
+        return 365;
+    }
+}
